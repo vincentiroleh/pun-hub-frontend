7ba85a5e1761b6782f200bcd020397d603bea894
Vincent Iroleh <irolehiroleh@gmail.com>
2020-10-07 17:26:40 +0100

    add google analytics

--- src/main.js
+++ src/main.js
@@ -1,4 +1,5 @@
 import Vue from 'vue';
+import VueAnalytics from 'vue-analytics';
 import App from './App.vue';
 import router from './router';
 
@@ -8,3 +9,8 @@ new Vue({
   router,
   render: (h) => h(App),
 }).$mount('#app');
+
+Vue.use(VueAnalytics, {
+  id: 'UA-179936735-1',
+  router,
+});
